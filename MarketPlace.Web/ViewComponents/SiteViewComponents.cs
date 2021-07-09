084618d22d5b32406006464d359459546d0eaa94
Sasy07 <sasan.ghafooryfard@gmail.com>
2021-07-10 03:52:41 +0430

    E08
    #Template
    #MainLayout
    #SiteHeader
    #SiteFooter

---
+++ MarketPlace.Web/ViewComponents/SiteViewComponents.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MarketPlace.Web.ViewComponents
+{
+    public class SiteHeaderViewComponent:ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("SiteHeader");
+        }
+    }
+    
+    
+    public class SiteFooterViewComponent:ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("SiteFooter");
+        }
+    }
+}
